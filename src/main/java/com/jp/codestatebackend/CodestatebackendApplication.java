0522656864df62ea9f10dd0ce4c9a9b608b33e99
JP <51864825+jp-chl@users.noreply.github.com>
2020-03-17 14:16:02 -0300

    1st version

---
+++ src/main/java/com/jp/codestatebackend/CodestatebackendApplication.java
@@ -0,0 +1,57 @@
+package com.jp.codestatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class CodestatebackendApplication {
+
+    final static String serverUrl1 = "https://gist.githubusercontent.com/PhantomGrin/a1e8ad30915ecd9d2659400d496d1ed6/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json";
+    final static String serverUrl2 = "https://gist.githubusercontent.com/PhantomGrin/a1e8ad30915ecd9d2659400d496d1ed6/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_titlecase.json";
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodestatebackendApplication.class, args);
+    }
+
+    public static String requestProcessedData(int urlId) {
+
+        String serverUrl = null;
+        if (urlId == 1) {
+
+            serverUrl = serverUrl1;
+        } else if (urlId == 2) {
+
+            serverUrl = serverUrl2;
+        } else {
+
+            return "ERROR";
+        }
+
+        RestTemplate request = new RestTemplate();
+        String result = request.getForObject(serverUrl, String.class);
+        System.out.println(serverUrl);
+        return (result);
+    } // end static String requestProcessedData(int urlId)
+
+    @GetMapping("/")
+    public static String hello() {
+        return "HELLO IM DATA READER";
+    } // end static String hello()
+
+    @GetMapping("/readDataForCode")
+    public static String requestCodeData() {
+
+        return requestProcessedData(1);
+    } // end static String requestCodeData()
+
+    @GetMapping("/readDataForState")
+    public static String requestStateData() {
+
+        return requestProcessedData(2);
+    } // end static String requestStateData()
+
+} // end class CodestatebackendApplication
